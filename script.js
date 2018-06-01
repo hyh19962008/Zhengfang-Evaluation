c9d7ec5046bcdcfe53524102355f72f47e9ea466
Demon <hyh19962008@gmail.com>
2018-06-01 03:29:30 +0000

    添加对多列(同一个课有多个老师)的支持

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function autoEval(){
     iframe=document.getElementById("iframeautoheight");
     itable=iframe.contentWindow.document.getElementById("trPjs");
     irows=itable.getElementsByTagName("select");
-    for(i=1;i<irows.length;i++){
+    for(i=0;i<irows.length;i++){
         if(i%8==0)
             irows[i].value=irows.options[2].value;
         else
